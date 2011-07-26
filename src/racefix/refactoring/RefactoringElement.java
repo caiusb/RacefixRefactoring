79e91c4001fa5ff4d7b6ba7377933d99d44de877
Caius Brindescu <caius.brindescu@gmail.com>
2011-07-25 19:03:42 -0500

    A very flimsy first implementation

---
+++ src/racefix/refactoring/RefactoringElement.java
@@ -0,0 +1,73 @@
+package racefix.refactoring;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.search.FieldDeclarationMatch;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
+import org.eclipse.ltk.core.refactoring.CheckConditionsOperation;
+import org.eclipse.ltk.core.refactoring.PerformRefactoringOperation;
+import org.eclipse.ltk.core.refactoring.Refactoring;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+
+public class RefactoringElement {
+
+  private final IField element;
+  private final Refactoring refactoring;
+
+  public RefactoringElement(String element, Refactoring refactoring) {
+    this.element = findField(element);
+    this.refactoring = refactoring;
+  }
+
+  private class Requestor extends SearchRequestor {
+
+    public IField field;
+
+    @Override
+    public void acceptSearchMatch(SearchMatch match) throws CoreException {
+      if (match instanceof FieldDeclarationMatch) {
+        FieldDeclarationMatch declarationMatch = (FieldDeclarationMatch) match;
+        Object rawElement = declarationMatch.getElement();
+        if (rawElement instanceof IField)
+          field = (IField) rawElement;
+      }
+    }
+  }
+  
+  public IField getField() {
+    return element;
+  }
+
+  private IField findField(String string) {
+    SearchEngine engine = new SearchEngine();
+    SearchPattern pattern = SearchPattern.createPattern(string, IJavaSearchConstants.FIELD,
+        IJavaSearchConstants.DECLARATIONS, SearchPattern.R_EXACT_MATCH);
+    IJavaSearchScope scope = SearchEngine.createWorkspaceScope();
+    Requestor requestor = new Requestor();
+
+    try {
+      engine.search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() }, scope, requestor,
+          new NullProgressMonitor());
+    } catch (CoreException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+    return requestor.field;
+  }
+  
+  public void apply() {
+    PerformRefactoringOperation operation = new PerformRefactoringOperation(refactoring, CheckConditionsOperation.ALL_CONDITIONS);
+    try {
+      operation.run(new NullProgressMonitor());
+    } catch (CoreException e) {
+    }
+  }
+}
--- src/racefix/refactoring/RefactoringEngine.java
+++ src/racefix/refactoring/RefactoringEngine.java
@@ -1,7 +1,43 @@
 package racefix.refactoring;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.search.FieldDeclarationMatch;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
+
+import edu.uiuc.threadlocalrefactoring.ThreadLocalRefactoring;
+
+import racefix.ClassChangeSet;
+import racefix.PrivatizeMethod;
+
 public class RefactoringEngine {
   
-  public RefactoringEngine() {
+  private final ClassChangeSet changeSet;
+
+  public RefactoringEngine(ClassChangeSet changeSet) {
+    this.changeSet = changeSet;
   }
+  
+  public void applyRefactorings() {
+    Set<PrivatizeMethod> privatizeMethods = changeSet.privatizeMethods;
+    Set<String> threadLocal = changeSet.threadLocal;
+    
+    for (String string : threadLocal) {
+      RefactoringElement element = new RefactoringElement(string, new ThreadLocalRefactoring());
+      
+    }
+  }
+  
+  
 }
