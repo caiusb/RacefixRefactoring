c3e3408887c7c0c80ba5fb0ecedddde244039dc6
Caius Brindescu <caius.brindescu@gmail.com>
2012-07-09 19:55:52 +0300

    Stub of make privatizable refactoring.
    So I can complete the top level logic of applying the refactorings.

---
+++ src/racefix/refactoring/makeprivatizable/MakePrivatizableRefactoring.java
@@ -0,0 +1,48 @@
+package racefix.refactoring.makeprivatizable;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.Refactoring;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+
+/**
+ * This refactoring makes a class privatizable
+ * 
+ * It implements the Privatizable interface and correctly privatizes the classes
+ * fields based on the information it receives from the analysis.
+ * 
+ * @author caius
+ * 
+ */
+public class MakePrivatizableRefactoring extends Refactoring {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RefactoringStatus checkInitialConditions(IProgressMonitor pm)
+			throws CoreException, OperationCanceledException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RefactoringStatus checkFinalConditions(IProgressMonitor pm)
+			throws CoreException, OperationCanceledException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Change createChange(IProgressMonitor pm) throws CoreException,
+			OperationCanceledException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
